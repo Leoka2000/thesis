4b3f06a35be3ca8d2bb8567471a747f999c1721a
Leoka2000 <reusoliveiraleo@gmail.com>
2024-11-08 17:43:17 +0100

    tabs

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -38,7 +38,37 @@ def lipases_view(request):
     return render(request, "pages/lipases.html", {})
 
 def hydrolases_view(request):
-    return render(request, "pages/hydrolases.html", {})
+    # Define the SMILES strings
+    pet_monomer_smiles = 'O=C(OCC)C1=CC=CC=C1C(=O)OCC'
+    pet_polymer_smiles = 'O=C(OCC)C1=CC=CC=C1C(=O)OCCC(=O)C2=CC=CC=C2C(=O)OCCC(=O)C3=CC=CC=C3C(=O)OCC'
+    vanillin_smiles = 'COC1=CC=C(C=C1)C=O'
+
+    # Generate and encode the image for the PET monomer
+    pet_monomer = Chem.MolFromSmiles(pet_monomer_smiles)
+    buffer = BytesIO()
+    pet_image = Draw.MolToImage(pet_monomer, size=(200, 200))
+    pet_image.save(buffer, format="PNG")
+    pet_img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    # Generate and encode the image for the PET polymer (4 monomers)
+    pet_polymer = Chem.MolFromSmiles(pet_polymer_smiles)
+    buffer = BytesIO()
+    pet_polymer_image = Draw.MolToImage(pet_polymer, size=(400, 400))
+    pet_polymer_image.save(buffer, format="PNG")
+    pet_polymer_img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    # Generate and encode the image for Vanillin
+    vanillin_molecule = Chem.MolFromSmiles(vanillin_smiles)
+    buffer = BytesIO()
+    vanillin_image = Draw.MolToImage(vanillin_molecule, size=(200, 200))
+    vanillin_image.save(buffer, format="PNG")
+    vanillin_img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    return render(request, "pages/hydrolases.html", {
+        "pet_image": pet_img_str,
+        "pet_polymer_image": pet_polymer_img_str,
+        "vanillin_image": vanillin_img_str
+    })
 
 def transferases_view(request):
     return render(request, "pages/transferases.html", {})
