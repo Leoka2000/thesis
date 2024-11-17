769f0c17dee956e7095ae77789389154a07d7dc1
Leoka2000 <reusoliveiraleo@gmail.com>
2024-11-17 15:58:32 +0100

    lipasestriolein

--- src/cfehome/views_lipases.py
+++ src/cfehome/views_lipases.py
@@ -8,25 +8,35 @@ from io import BytesIO
 def lipases_view(request):
     # Define SMILES strings for reagents and products (First reaction)
     reagents = {
-        "POP Palm oil mid fraction": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O",  # IUPAC: 2,3-bis(hexadecanoyloxy)propyl (Z)-octadec-9-enoate
-        "Stearic Acid": "C(CCCCCCCCCCCCCCCCC)(=O)O",  # IUPAC: octadecanoic acid
+        "POP Palm oil mid fraction": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O",
+        "Stearic Acid": "C(CCCCCCCCCCCCCCCCC)(=O)O",
     }
     products = {
-        "P-OSt - Cocoa butter substute": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(C(C)O)OC(CCCCCCCCCCCCCCCCC)=O",  # IUPAC: 2-hydroxy-1-(octadecanoyloxy)propyl (Z)-octadec-9-enoate
-        "St-OSt": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCCCC)=O",
+        "P-OSt - Cocoa butter substitute": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(C(C)O)OC(CCCCCCCCCCCCCCCCC)=O",
+        "St-O-St": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCCCC)=O",
         "Palmitate": "C(CCCCCCCCCCCCCCC)(=O)[O-]",
     }
 
-    # Define second set of SMILES strings for reagents and products (Second reaction)
+    # Second reaction
     second_cbs_reaction_agents = {
         "POP Palm oil mid fraction": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O",
-        "St-St-St": "C(CCCCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCCCC)=O",  # Additional reagent
+        "St-St-St": "C(CCCCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCCCC)=O",
     }
     second_cbs_reaction_products = {
-        "P-OSt - Cocoa butter substute": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(C(C)O)OC(CCCCCCCCCCCCCCCCC)=O",
-        "St-O-St": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCCCC)=O",
+        "P-OSt - Cocoa butter substitute": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(C(C)O)OC(CCCCCCCCCCCCCCCCC)=O",
+        "St-O-St": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCCCCO)COC(CCCCCCCCCCCCCCCCC)=O",
         "P-St-St": "C(CCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCCCC)=O",
-        "P-St-P": "C(CCCCCCCCCCCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCC)=O",  # Additional product
+        "P-St-P": "C(CCCCCCCCCCCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCC)=O",
+    }
+
+    # HMS Triolein Reaction
+    hms_triolein_reagents = {
+        "palm_top_fraction (tripalmitin)": "C(CCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O",
+        "Triolein": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OCC(COC(CCCCCCC\C=C/CCCCCCCC)=O)OC(CCCCCCC\C=C/CCCCCCCC)=O",
+    }
+    hms_triolein_products = {
+        "HMS (O-P-O)": "C(CCCCCCCCCCCCCCC)(=O)OC(COC(CCCCCCC\C=C/CCCCCCCC)=O)COC(CCCCCCC\C=C/CCCCCCCC)=O",
+        "P-O-P": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC[C@@H](COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O",
     }
 
     def process_molecules(molecule_dict):
@@ -55,18 +65,23 @@ def lipases_view(request):
         organic_acid = request.GET.get("organic_fatty_acid")
         catalyser = request.GET.get("catalyser")
 
-        # Check if selected values match the expected combination for the second reaction
         if (
+            palm_oil == "palm_top_oil"
+            and organic_acid == "triolein"
+            and catalyser == "1_3_specific_lipase"
+        ):
+            reagents_data = process_molecules(hms_triolein_reagents)
+            products_data = process_molecules(hms_triolein_products)
+            messages.success(request, "HMS-Triolein reaction successful! The reagents and products are displayed.")
+        elif (
             palm_oil == "palm_oil_midfraction"
             and organic_acid == "stearic_acid_3st"
             and catalyser == "1_3_specific_lipase"
         ):
-            # Process molecules for the second reaction
             reagents_data = process_molecules(second_cbs_reaction_agents)
             products_data = process_molecules(second_cbs_reaction_products)
             messages.success(request, "Reaction successful! The reagents and products are displayed.")
         else:
-            # Process molecules for the first reaction (default)
             reagents_data = process_molecules(reagents)
             products_data = process_molecules(products)
             messages.success(request, "Reaction successful! The reagents and products are displayed.")
@@ -75,3 +90,27 @@ def lipases_view(request):
         "reagents_data": reagents_data,
         "products_data": products_data,
     })
+
+
+
+  # Define SMILES strings for reagents and products
+    # reagents = {
+    #     #POP= palm oil mid fraction
+    #     "POP Palm oil mid fraction": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O", #iupac 2,3-bis(hexadecanoyloxy)propyl (Z)-octadec-9-enoate
+    #     "Stearic Acid": "C(CCCCCCCCCCCCCCCCC)(=O)O", #IUPAC octadecanoic acid
+    #     "palm_top_fraction (tripalmitin)": "C(CCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O", #IUPAC 2,3-bis(hexadecanoyloxy)propyl hexadecanoate
+    #     #acima fiz qnd tava doente e arrumei
+    #     "Triolein": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OCC(COC(CCCCCCC\C=C/CCCCCCCC)=O)OC(CCCCCCC\C=C/CCCCCCCC)=O", #IUPAC 2,3-bis((Z)-octadec-9-enoyloxy)propyl (Z)-octadec-9-enoate
+    #     "oleic-acid": "C(CCCCCCC\C=C/CCCCCCCC)(=O)O", #iupac (Z)-octadec-9-enoic acid
+    # }
+    # products = {
+    #     "P-OSt - CBS - (2-stearoyl-1,3-dihydroxypropane)": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(C(C)O)OC(CCCCCCCCCCCCCCCCC)=O", #IUPAC: 2-hydroxy-1-(octadecanoyloxy)propyl (Z)-octadec-9-enoate
+    #     "St-OSt (1,3-distearoyl-2-oleylglycerol)": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(C(COC(CCCCCCCCCCCCCCCCC)=O)O)OC(CCCCCCCCCCCCCCCCC)=O", # IUPAC 2-hydroxy-1,3-bis(octadecanoyloxy)propyl (Z)-octadec-9-enoate
+    #     "St-O-St": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCCCC)=O",#2-((Z)-octadec-9-enoyloxy)-1,3-bis(octadecanoyloxy)propane
+    #      "P": "C(CCCCCCCCCCCCCCC)(=O)[O-]", #hexadecanoate 
+    #     "P-St-St": "C(CCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCCCC)=O", #IUPAC: 2,3-bis(octadecanoyloxy)propyl hexadecanoate
+    #     "P-St-P": "C(CCCCCCCCCCCCCCCCC)(=O)OC(COC(CCCCCCCCCCCCCCC)=O)COC(CCCCCCCCCCCCCCC)=O", #IUPAC: 2-(octadecanoyloxy)-1,3-bis(hexadecanoyloxy)propane
+    #     "HMS (O-P-O)": "C(CCCCCCCCCCCCCCC)(=O)OC(COC(CCCCCCC\C=C/CCCCCCCC)=O)COC(CCCCCCC\C=C/CCCCCCCC)=O", #iupac 2-(hexadecanoyloxy)-1,3-bis((Z)-octadec-9-enoyloxy)propane
+    #     "P-O-P": "C(CCCCCCC\C=C/CCCCCCCC)(=O)OC[C@@H](COC(CCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCC)=O", #IUPAC: (2R)-3-(hexadecanoyloxy)-2-(hexadecanoyloxy)propyl (9Z)-octadec-9-enoate
+    #     "St-St-St": "C(CCCCCCCCCCCCCCCCC)(=O)OCC(COC(CCCCCCCCCCCCCCCCC)=O)OC(CCCCCCCCCCCCCCCCC)=O"  #a trygliceride with three stereate group IUPAC: 2,3-bis(octadecanoyloxy)propyl octadecanoate
+    # }
